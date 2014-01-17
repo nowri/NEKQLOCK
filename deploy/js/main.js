7d02398c24b2e52b00549b4106934029c298bdce
nowri <interactive@nowri.in>
2014-01-18 00:20:13 +0900

    Update

--- deploy/js/main.js
+++ deploy/js/main.js
@@ -1,6 +1,6 @@
-//NEKQLOCK v1.28
+//NEKQLOCK v1.29
 
-var VERSION = 1.28,
+var VERSION = 1.29,
 	FILTERS = [
 		"194AMb3",
 		"1hy9EFj",
@@ -998,7 +998,12 @@ var VERSION = 1.28,
 		"6BIy26U256pUI",
 		"12olBDpsJhoASI",
 		"gd13g6mlPyGuk",
-		"CVjD8t7UyHBPa"
+		"CVjD8t7UyHBPa",
+		"13CYOfhbxFZvEc",
+		"12DlOl7N0hj8Qg",
+		"10uziIkvI40bGE",
+		"2x205rfgpdtK0",
+		"qBPjiENGe8Vz2"
 	];
 
 //timer
